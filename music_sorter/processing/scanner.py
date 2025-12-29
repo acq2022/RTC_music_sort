a95dbdba11b64e34dd35cebdedabea428bec5452
acq2022 <113108275+acq2022@users.noreply.github.com>
2025-12-29 23:26:10 +0100

    ignored folders fixed

--- music_sorter/processing/scanner.py
+++ music_sorter/processing/scanner.py
@@ -2,15 +2,18 @@ from pathlib import Path
 from config import SUPPORTED_EXTENSIONS
 
 class Scanner:
-    
+
     def scan(self, root: Path, ignored_dirs=None):
         ignored_dirs = set(ignored_dirs or [])
         files = []
 
-        for path in root.rglob("*"):
-            if path.is_dir() and path.name in ignored_dirs:
-                continue
-            if path.suffix.lower() in SUPPORTED_EXTENSIONS:
-                files.append(path)
+        for path in root.iterdir():
+            if path.is_dir():
+                if path.name in ignored_dirs:
+                    continue  # on n'entre pas dans ce dossier
+                files.extend(self.scan(path, ignored_dirs))
+            else:
+                if path.suffix.lower() in SUPPORTED_EXTENSIONS:
+                    files.append(path)
 
         return files
