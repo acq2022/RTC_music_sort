fb789591165711d1c83a1ff3293a2ae93f8d533b
acq2022 <113108275+acq2022@users.noreply.github.com>
2026-01-06 00:18:13 +0100

    remplissage (DiscogsEnricher) done ! TODO : fix the   None - (0) Unknown Folder    file name

--- music_sorter/services/discogs/discogs_enricher.py
+++ music_sorter/services/discogs/discogs_enricher.py
@@ -4,23 +4,27 @@ from domain.album import Album
 
 logger = logging.getLogger("DiscogsEnricher")
 
-# TODO : paufiner le remplissage (+ de paramètres à remplir dans Album)
 class DiscogsEnricher:
     def apply(self, album: Album, result) -> None:
         logger.info(f"[Discogs] Applying result {result.id} to '{album.title}'")
-
-        album.artists = result.artists
-        album.year = result.year
-        album.title = result.title
-
-        label_infos = result.data["labels"][0]
 
         def clean(value):
             if value is None or value.lower() == "none":
                 return None
             return value
-        
+
+        album.title = result.title
+        album.album_id = result.id
+        album.artists = result.artists
+        album.year = result.year
+
+        label_infos = result.data["labels"][0]
         album.label = clean(label_infos.get("name"))
         album.catno = clean(label_infos.get("catno"))
-        
+
+        album.total_tracks = clean(str(len(result.tracklist)))
         album.style = result.styles
+        album.website = clean(result.url)
+        album.original_date = clean(result.data["released"])
+        album.release_country = clean(result.country)
+
